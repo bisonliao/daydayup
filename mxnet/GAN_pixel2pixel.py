ed86ecfc0ce1a21a465234021d834159c6d63972
bisonliao <11228490@qq.com>
2019-06-24 11:27:28 +0800

    x

---
+++ mxnet/GAN_pixel2pixel.py
@@ -0,0 +1,425 @@
+'''
+使用GAN，输入卫星图片，输出地图
+
+对于NetG，输入卫星图片，输出地图
+对于NetD，输入卫星图片+地图在通道维度的叠加，输出真伪判断。
+对于NetD，标注的真数据是一组卫星图片和对应的地图， NetG生成的是伪数据
+'''
+import os
+import matplotlib as mpl
+import tarfile
+import matplotlib.image as mpimg
+from matplotlib import pyplot as plt
+from datetime import datetime
+import time
+import logging
+import mxnet as mx
+from mxnet import gluon
+from mxnet import ndarray as nd
+from mxnet.gluon import nn, utils
+from mxnet.gluon.nn import Dense, Activation, Conv2D, Conv2DTranspose, \
+    BatchNorm, LeakyReLU, Flatten, HybridSequential, HybridBlock, Dropout
+from mxnet import autograd
+import numpy as np
+import math
+import imageio
+
+#######################################################################
+# arguments:
+epochs = 100
+batch_size = 10
+
+use_gpu = True
+ctx = mx.gpu() if use_gpu else mx.cpu()
+
+lr = 0.0002
+beta1 = 0.5
+lambda1 = 100
+pool_size = 50
+
+#######################################################################
+# define dataset
+dataset = 'maps'
+img_wd = 256
+img_ht = 256
+train_img_path = 'E:\\DeepLearning\\data\\CGAN\\%s/train' % (dataset)
+val_img_path = 'E:\\DeepLearning\\data\\CGAN\\%s/val' % (dataset)
+
+# 直接加载到内存，占用太多内存，我的机器配置不够
+'''
+def load_data(path, batch_size, is_reversed=False):
+    img_in_list = []
+    img_out_list = []
+    for path, _, fnames in os.walk(path):
+        for fname in fnames:
+            if not fname.endswith('.jpg'):
+                continue
+            img = os.path.join(path, fname)
+            img_arr = mx.image.imread(img).astype(np.float32)/127.5 - 1
+            img_arr = mx.image.imresize(img_arr, img_wd * 2, img_ht)
+            # Crop input and output images
+            img_arr_in, img_arr_out = [mx.image.fixed_crop(img_arr, 0, 0, img_wd, img_ht),
+                                       mx.image.fixed_crop(img_arr, img_wd, 0, img_wd, img_ht)]
+            img_arr_in, img_arr_out = [nd.transpose(img_arr_in, (2,0,1)),
+                                       nd.transpose(img_arr_out, (2,0,1))]
+            img_arr_in, img_arr_out = [img_arr_in.reshape((1,) + img_arr_in.shape),
+                                       img_arr_out.reshape((1,) + img_arr_out.shape)]
+            img_in_list.append(img_arr_out if is_reversed else img_arr_in)
+            img_out_list.append(img_arr_in if is_reversed else img_arr_out)
+            
+
+    return mx.io.NDArrayIter(data=[nd.concat(*img_in_list, dim=0), nd.concat(*img_out_list, dim=0)],
+                             batch_size=batch_size)
+train_data = load_data(train_img_path, batch_size, is_reversed=True)
+val_data = load_data(val_img_path, batch_size, is_reversed=True)
+'''
+class MyFileDataset(mx.gluon.data.Dataset):
+    def __init__(self, path,is_reversed=True ):
+        super(MyFileDataset, self)
+        self.is_reversed = is_reversed
+        self.filelist=[]
+        for path, _, fnames in os.walk(path):
+            for fname in fnames:
+                if not fname.endswith('.jpg'):
+                    continue
+                img = os.path.join(path, fname)
+                self.filelist.append(img)
+
+        return
+
+    def __getitem__(self, item):
+        img = self.filelist[item]
+        img_arr = mx.image.imread(img).astype(np.float32) / 127.5 - 1
+        img_arr = mx.image.imresize(img_arr, img_wd * 2, img_ht)
+        # Crop input and output images
+        img_arr_in = mx.image.fixed_crop(img_arr, 0, 0, img_wd, img_ht)
+        img_arr_out = mx.image.fixed_crop(img_arr, img_wd, 0, img_wd, img_ht)
+        img_arr_in  = nd.transpose(img_arr_in, (2, 0, 1))
+        img_arr_out  =  nd.transpose(img_arr_out, (2, 0, 1))
+        #img_arr_in = img_arr_in.reshape((1,) + img_arr_in.shape)
+        #img_arr_out= img_arr_out.reshape((1,) + img_arr_out.shape)
+        i = img_arr_out if self.is_reversed else img_arr_in
+        o = img_arr_in if self.is_reversed else img_arr_out
+        return i,o
+    def __len__(self):
+        return len(self.filelist)
+
+train_data = gluon.data.DataLoader(MyFileDataset(train_img_path,True), batch_size=batch_size, shuffle=False, last_batch='discard')
+val_data = gluon.data.DataLoader(MyFileDataset(val_img_path,True), batch_size=batch_size, shuffle=False, last_batch='discard')
+
+def visualize(img_arr):
+    plt.imshow(((img_arr.asnumpy().transpose(1, 2, 0) + 1.0) * 127.5).astype(np.uint8))
+    plt.axis('off')
+
+def preview_train_data():
+    #img_in_list, img_out_list = train_data.next().data
+    for _, (img_in_list, img_out_list) in enumerate(train_data):
+        for i in range(4):
+            plt.subplot(2,4,i+1)
+            visualize(img_in_list[i])
+            plt.subplot(2,4,i+5)
+            visualize(img_out_list[i])
+        plt.show()
+        break
+
+preview_train_data()
+
+#######################################################################
+# Define Unet generator skip block
+class UnetSkipUnit(HybridBlock):
+    def __init__(self, inner_channels, outer_channels, inner_block=None, innermost=False, outermost=False,
+                 use_dropout=False, use_bias=False):
+        super(UnetSkipUnit, self).__init__()
+
+        with self.name_scope():
+            self.outermost = outermost
+            en_conv = Conv2D(channels=inner_channels, kernel_size=4, strides=2, padding=1,
+                             in_channels=outer_channels, use_bias=use_bias)
+            en_relu = LeakyReLU(alpha=0.2)
+            en_norm = BatchNorm(momentum=0.1, in_channels=inner_channels)
+            de_relu = Activation(activation='relu')
+            de_norm = BatchNorm(momentum=0.1, in_channels=outer_channels)
+
+            if innermost:
+                de_conv = Conv2DTranspose(channels=outer_channels, kernel_size=4, strides=2, padding=1,
+                                          in_channels=inner_channels, use_bias=use_bias)
+                encoder = [en_relu, en_conv]
+                decoder = [de_relu, de_conv, de_norm]
+                model = encoder + decoder
+            elif outermost:
+                de_conv = Conv2DTranspose(channels=outer_channels, kernel_size=4, strides=2, padding=1,
+                                          in_channels=inner_channels * 2)
+                encoder = [en_conv]
+                decoder = [de_relu, de_conv, Activation(activation='tanh')]
+                model = encoder + [inner_block] + decoder
+            else:
+                de_conv = Conv2DTranspose(channels=outer_channels, kernel_size=4, strides=2, padding=1,
+                                          in_channels=inner_channels * 2, use_bias=use_bias)
+                encoder = [en_relu, en_conv, en_norm]
+                decoder = [de_relu, de_conv, de_norm]
+                model = encoder + [inner_block] + decoder
+            if use_dropout:
+                model += [Dropout(rate=0.5)]
+
+            self.model = HybridSequential()
+            with self.model.name_scope():
+                for block in model:
+                    self.model.add(block)
+
+    def hybrid_forward(self, F, x):
+        if self.outermost:
+            return self.model(x)
+        else:
+            return F.concat(self.model(x), x, dim=1)
+
+# Define Unet generator
+class UnetGenerator(HybridBlock):
+    def __init__(self, in_channels, num_downs, ngf=64, use_dropout=True):
+        super(UnetGenerator, self).__init__()
+
+        #Build unet generator structure
+        unet = UnetSkipUnit(ngf * 8, ngf * 8, innermost=True)
+        for _ in range(num_downs - 5):
+            unet = UnetSkipUnit(ngf * 8, ngf * 8, unet, use_dropout=use_dropout)
+        unet = UnetSkipUnit(ngf * 8, ngf * 4, unet)
+        unet = UnetSkipUnit(ngf * 4, ngf * 2, unet)
+        unet = UnetSkipUnit(ngf * 2, ngf * 1, unet)
+        unet = UnetSkipUnit(ngf, in_channels, unet, outermost=True)
+
+        with self.name_scope():
+            self.model = unet
+
+    def hybrid_forward(self, F, x):
+        return self.model(x)
+
+# Define the PatchGAN discriminator
+class Discriminator(HybridBlock):
+    def __init__(self, in_channels, ndf=64, n_layers=3, use_sigmoid=False, use_bias=False):
+        super(Discriminator, self).__init__()
+
+        with self.name_scope():
+            self.model = HybridSequential()
+            kernel_size = 4
+            padding = int(np.ceil((kernel_size - 1)/2))
+            self.model.add(Conv2D(channels=ndf, kernel_size=kernel_size, strides=2,
+                                  padding=padding, in_channels=in_channels))
+            self.model.add(LeakyReLU(alpha=0.2))
+
+            nf_mult = 1
+            for n in range(1, n_layers):
+                nf_mult_prev = nf_mult
+                nf_mult = min(2 ** n, 8)
+                self.model.add(Conv2D(channels=ndf * nf_mult, kernel_size=kernel_size, strides=2,
+                                      padding=padding, in_channels=ndf * nf_mult_prev,
+                                      use_bias=use_bias))
+                self.model.add(BatchNorm(momentum=0.1, in_channels=ndf * nf_mult))
+                self.model.add(LeakyReLU(alpha=0.2))
+
+            nf_mult_prev = nf_mult
+            nf_mult = min(2 ** n_layers, 8)
+            self.model.add(Conv2D(channels=ndf * nf_mult, kernel_size=kernel_size, strides=1,
+                                  padding=padding, in_channels=ndf * nf_mult_prev,
+                                  use_bias=use_bias))
+            self.model.add(BatchNorm(momentum=0.1, in_channels=ndf * nf_mult))
+            self.model.add(LeakyReLU(alpha=0.2))
+            self.model.add(Conv2D(channels=1, kernel_size=kernel_size, strides=1,
+                                  padding=padding, in_channels=ndf * nf_mult))
+            if use_sigmoid:
+                self.model.add(Activation(activation='sigmoid'))
+
+    def hybrid_forward(self, F, x):
+        out = self.model(x)
+        #print(out)
+        return out
+#######################################################################
+# init network
+def param_init(param):
+    if param.name.find('conv') != -1:
+        if param.name.find('weight') != -1:
+            param.initialize(init=mx.init.Normal(0.02), ctx=ctx)
+        else:
+            param.initialize(init=mx.init.Zero(), ctx=ctx)
+    elif param.name.find('batchnorm') != -1:
+        param.initialize(init=mx.init.Zero(), ctx=ctx)
+        # Initialize gamma from normal distribution with mean 1 and std 0.02
+        if param.name.find('gamma') != -1:
+            param.set_data(nd.random_normal(1, 0.02, param.data().shape))
+
+def network_init(net):
+    for param in net.collect_params().values():
+        param_init(param)
+
+def set_network():
+    # Pixel2pixel networks
+    netG = UnetGenerator(in_channels=3, num_downs=8)
+    netD = Discriminator(in_channels=6)
+
+    # Initialize parameters
+    network_init(netG)
+    network_init(netD)
+
+    # trainer for the generator and the discriminator
+    trainerG = gluon.Trainer(netG.collect_params(), 'adam', {'learning_rate': lr, 'beta1': beta1})
+    trainerD = gluon.Trainer(netD.collect_params(), 'adam', {'learning_rate': lr, 'beta1': beta1})
+
+    return netG, netD, trainerG, trainerD
+
+# Loss
+GAN_loss = gluon.loss.SigmoidBinaryCrossEntropyLoss()
+L1_loss = gluon.loss.L1Loss()
+
+netG, netD, trainerG, trainerD = set_network()
+#######################################################################
+# We use history image pool to help discriminator memorize history errors
+# instead of just comparing current real input and fake output.
+# WHY ？
+class ImagePool():
+    def __init__(self, pool_size):
+        self.pool_size = pool_size
+        if self.pool_size > 0:
+            self.num_imgs = 0
+            self.images = []
+
+    def query(self, images):
+        if self.pool_size == 0:
+            return images
+        ret_imgs = []
+        for i in range(images.shape[0]):
+            image = nd.expand_dims(images[i], axis=0)
+            if self.num_imgs < self.pool_size:   #历史库还有空位，就填充到历史库
+                self.num_imgs = self.num_imgs + 1
+                self.images.append(image)
+                ret_imgs.append(image)
+            else:
+                p = nd.random_uniform(0, 1, shape=(1,)).asscalar()
+                if p > 0.5: # 一定概率取历史库，且更新历史库
+                    random_id = nd.random_uniform(0, self.pool_size - 1, shape=(1,)).astype(np.uint8).asscalar()
+                    tmp = self.images[random_id].copy()
+                    self.images[random_id] = image
+                    ret_imgs.append(tmp)
+                else: #一定概率取当期的数据
+                    ret_imgs.append(image)
+        ret_imgs = nd.concat(*ret_imgs, dim=0)
+        return ret_imgs
+
+
+
+#######################################################################
+#train network
+def save_image(data, epoch, image_size, batch_size, output_dir, padding=2):
+    """ save image """
+    data = data.asnumpy().transpose((0, 2, 3, 1))
+    datanp = np.clip(
+        (data - np.min(data))*(255.0/(np.max(data) - np.min(data))), 0, 255).astype(np.uint8)
+    x_dim = min(8, batch_size)
+    y_dim = int(math.ceil(float(batch_size) / x_dim))
+    height, width = int(image_size + padding), int(image_size + padding)
+    grid = np.zeros((height * y_dim + 1 + padding // 2, width *
+                     x_dim + 1 + padding // 2, 3), dtype=np.uint8)
+    k = 0
+    for y in range(y_dim):
+        for x in range(x_dim):
+            if k >= batch_size:
+                break
+            start_y = y * height + 1 + padding // 2
+            end_y = start_y + height - padding
+            start_x = x * width + 1 + padding // 2
+            end_x = start_x + width - padding
+            np.copyto(grid[start_y:end_y, start_x:end_x, :], datanp[k])
+            k += 1
+    imageio.imwrite(
+        '{}/fake_samples_epoch_{}.png'.format(output_dir, epoch), grid)
+
+def facc(label, pred): # custom acc compare function
+    pred = pred.ravel()
+    label = label.ravel()
+    return ((pred > 0.5) == label).mean()
+def train():
+    image_pool = ImagePool(pool_size)
+    metric = mx.metric.CustomMetric(facc)
+
+    stamp = datetime.now().strftime('%Y_%m_%d-%H_%M')
+    logging.basicConfig(level=logging.DEBUG)
+
+    for epoch in range(epochs):
+        tic = time.time()   # begin time of epoch
+        btic = time.time() # begin time of iterator
+
+        iter = 0
+        #for batch in train_data:
+        for i, (real_in, real_out) in enumerate(train_data):
+            ############################
+            # (1) Update D network: maximize log(D(x, y)) + log(1 - D(x, G(x, z)))
+            ###########################
+            #real_in = batch.data[0].as_in_context(ctx)
+            #real_out = batch.data[1].as_in_context(ctx)
+            real_in = real_in.as_in_context(ctx)
+            real_out = real_out.as_in_context(ctx)
+
+
+            fake_out = netG(real_in)
+            fake_concat = image_pool.query(nd.concat(real_in, fake_out, dim=1))
+            with autograd.record():
+                # Train with fake image
+                # Use image pooling to utilize history images
+                output = netD(fake_concat)
+                fake_label = nd.zeros(output.shape, ctx=ctx)
+                errD_fake = GAN_loss(output, fake_label)
+                metric.update([fake_label, ], [output, ])
+
+                # Train with real image
+                real_concat = nd.concat(real_in, real_out, dim=1)
+                output = netD(real_concat)
+                real_label = nd.ones(output.shape, ctx=ctx)
+                errD_real = GAN_loss(output, real_label)
+                errD = (errD_real + errD_fake) * 0.5
+                errD.backward()
+                metric.update([real_label, ], [output, ])
+
+            trainerD.step(real_in.shape[0])
+
+            ############################
+            # (2) Update G network: maximize log(D(x, G(x, z))) - lambda1 * L1(y, G(x, z))
+            ###########################
+            with autograd.record():
+                fake_out = netG(real_in)
+                fake_concat = nd.concat(real_in, fake_out, dim=1)
+                output = netD(fake_concat)
+                real_label = nd.ones(output.shape, ctx=ctx)
+                errG = GAN_loss(output, real_label) + L1_loss(real_out, fake_out) * lambda1
+                errG.backward()
+
+            trainerG.step(real_in.shape[0])
+
+            # Print log infomation every ten batches
+            if iter % 10 == 0:
+                name, acc = metric.get()
+                logging.info('speed: {} samples/s'.format(batch_size / (time.time() - btic)))
+                logging.info(
+                    'discriminator loss = %f, generator loss = %f, binary training acc = %f at iter %d epoch %d'
+                    % (nd.mean(errD).asscalar(),
+                       nd.mean(errG).asscalar(), acc, iter, epoch))
+            iter = iter + 1
+            btic = time.time()
+
+
+        name, acc = metric.get()
+        metric.reset()
+        logging.info('\nbinary training acc at epoch %d: %s=%f' % (epoch, name, acc))
+        logging.info('time: %f' % (time.time() - tic))
+
+        # Visualize one generated image for each epoch
+        '''
+        fake_img = fake_out[0]
+        visualize(fake_img)
+        plt.show()
+        '''
+        if epoch == 0:
+            save_image(real_in, epoch, img_wd, batch_size, "./data")
+        else:
+            save_image(fake_out, epoch, img_wd, batch_size, "./data")
+
+
+train()
+netG.save_parameters("./data/CGAN_netG.param")
+netD.save_parameters("./data/CGAN_netD.param")

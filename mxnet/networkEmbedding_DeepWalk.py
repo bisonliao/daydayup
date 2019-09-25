7fd70a8a51ecdf05e31e5fe6d06f23fa1ad2c39d
bisonliao <11228490@qq.com>
2019-09-25 11:58:56 +0800

    x

--- mxnet/networkEmbedding_DeepWalk.py
+++ mxnet/networkEmbedding_DeepWalk.py
@@ -4,8 +4,9 @@
 DeepWalk的第二步：n-gram 算法进行词向量学习，使用的是fasttext
 
 同时使用开源的DeepWalk命令行（https://github.com/phanein/deepwalk）另外
-生成network embedding做AB对比，发现节点58的两种算法下的最近节点没有相交，我的实现看来有问题。、
-另外fasttext训练过程中，loss值没有最终收敛到很小。
+生成network embedding做AB对比，发现节点58的两种算法下的最近节点没有相交，我的实现可能有问题。通过检查相似用户点的jaccard相似度，两个算法没有明显差距
+用一个小的网络验证，通过图形展示节点位置、聚类的结果可以看出该算法和代码是有效的。
+fasttext训练过程中，lr等几个参数特别关键，需要注意。
 '''
 # coding:utf8
 
@@ -19,9 +20,12 @@ import matplotlib.pyplot as plt
 import os
 
 from sklearn.neighbors import BallTree
+from  sklearn.cluster import DBSCAN
+from  sklearn.cluster import KMeans
 
 DATA_DIR= "E:\\DeepLearning\\data\\network_data\\BlogCatalog-dataset\\data\\"
-DIM=100
+DIM=2
+TEST_USER=2
 
 # 把csv数据加载为二维array，是一个稀疏矩阵
 def loadData():
@@ -32,9 +36,9 @@ def loadData():
             if int(row[0]) > maxUserId:
                 maxUserId = int(row[0])
     print("maxUserId:", maxUserId)
-    #maxUserId = 300
+    maxUserId = 14 #小网络直观验证开启
     edges = lil_matrix((maxUserId+1, maxUserId+1), dtype="uint8")
-    with open(DATA_DIR+"edges.csv", "r", encoding="utf8") as f:
+    with open(DATA_DIR+"edges2.csv", "r", encoding="utf8") as f:
         reader = csv.reader(f)
         for row in reader:
             if int(row[0]) > maxUserId or int(row[1]) > maxUserId:
@@ -53,6 +57,7 @@ def geneCorpusFromEdges(edges:lil_matrix):
     for i in range(EXAMP_NUM):
         r = random.randint(1, maxUserId)
         user = r
+
         wordList = list()
         wordList.append("u%d"%user)
         for j in range(WORD_NUM):
@@ -62,6 +67,8 @@ def geneCorpusFromEdges(edges:lil_matrix):
                 break
             index = random.randint(0, len(indices)-1)
             user = indices[index]
+            if len(wordList) > 1 and user == wordList[-2]:
+                continue
             wordList.append("u%d" % user)
         example_list.append(" ".join(wordList))
         print("\rgenerate example process %.2f%%"%(i*100/EXAMP_NUM), end='')
@@ -78,32 +85,46 @@ def cosDist(v1:np.ndarray,v2:np.ndarray):
     return 1 - np.dot(v1,v2)/(np.linalg.norm(v1,ord=2)*np.linalg.norm(v2, ord=2))
 
 def test(model, edges):
-    w = "u58"
-    A = set(edges[int(w[1:])].nonzero()[1])
-    nn = list(model.get_nearest_neighbors(w))
+    w = "u"+str(TEST_USER)
+    A = set(edges[TEST_USER].nonzero()[1])
+    nn = list(model.get_nearest_neighbors(w, k=5))
     print(nn)
-    return [neighbor[1] for neighbor in nn]
-    '''for neighbor in nn:
+    cluster(model,edges)
+
+    for neighbor in nn:
         neighbor = int(neighbor[1][1:])
         B = set(edges[neighbor].nonzero()[1])
         print("%d \tsim:%f"%(neighbor, jaccardSimilarity(A,B)), end='')
         print("\t", B)
-
     if DIM == 2 and edges.shape[0] < 1000:
-        graph = from_scipy_sparse_matrix(edges)
-        embedding = np.zeros((edges.shape[0], DIM) )
-        for i in range(edges.shape[0]):
+        graph = from_scipy_sparse_matrix(edges[1:,1:])
+        embedding = np.zeros((edges.shape[0]-1, DIM) )
+        for i in range(edges.shape[0]-1):
             embedding[i] = model.get_word_vector("u"+str(i+1))
-        draw(graph, pos=embedding)
-        plt.show()'''
+        draw(graph, pos=embedding, with_labels=True)
+        plt.show()
+
+def cluster(model:fasttext.FastText._FastText,edges):
+    m = np.zeros((edges.shape[0]-1, DIM))
+    for i in range(edges.shape[0]-1):
+        w = "u"+str(i+1)
+        m[i] = model.get_word_vector(w)
+    cl = DBSCAN(min_samples=3, metric='cosine', eps=0.05)
+    #cl = KMeans(n_clusters=3)
+    print(cl.fit_predict(m))
+
+
 
 
 edges = loadData()  # type:lil_matrix
 print("edges number:", edges.nnz)
-if False:
-    #geneCorpusFromEdges(edges)
-    #print("geneCorpusFromEdges() done!")
-    model = fasttext.train_unsupervised("./data/ne_corpus.txt", epoch = 200, lr=0.0001, dim=DIM) # type:fasttext.FastText._FastText
+if True:
+    geneCorpusFromEdges(edges)
+    print("geneCorpusFromEdges() done!")
+    #model = fasttext.train_unsupervised("./data/ne_corpus.txt", epoch = 200, lr=0.0001, dim=DIM,maxn=0) # type:fasttext.FastText._FastText
+    #model = fasttext.train_unsupervised("./data/ne_corpus.txt", epoch=100000, lr=0.0001, dim=DIM, maxn=0)  # type:fasttext.FastText._FastText
+    model = fasttext.train_unsupervised("./data/ne_corpus.txt", epoch=100, lr=0.1, dim=DIM,
+                                        maxn=0)  # type:fasttext.FastText._FastText
     model.save_model("./data/network_embedding.bin")
     print(type(model))
 
@@ -111,37 +132,6 @@ model = fasttext.load_model("./data/network_embedding.bin")
 A = test(model, edges)
 
 
-# 用开源的命令行工具生成embeding，加载到BallTree里，查询相似节点
-def deepwalk(edges):
-    embfile = "./data/network.emb" #这个文件是用命令行生成的embedding结果
-    f = open(embfile, "r", encoding="utf8")
-    cnt = 0
-    vects=dict() #读到字典里
-    for line in f:
-        cnt += 1
-        if cnt == 1:
-            continue
-
-        if line[-1] =="\n":
-            line = line[:-1]
-        if line[-1] == "\r":
-            line = line[:-1]
-
-        fields = line.split(" ")
-        vects[int(fields[0])] = [ float(v) for v in fields[1:] ]
-    f.close()
-    #把字典转为向量列表
-    m = np.zeros((len(vects.keys())+1, DIM), dtype="float32")
-    for i in range(len(vects.keys())):
-        m[i+1] = np.array(vects[i+1])
-
-    tree = BallTree(m, metric="pyfunc", func=cosDist)
-    neighbors = tree.query([m[58]], k=11)
-    print(neighbors)
-    return neighbors[1][0]
-
-B = deepwalk(edges)
-print(jaccardSimilarity(set(A),set(B) ))
 
 
 
